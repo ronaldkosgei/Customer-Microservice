268fa0a383b5e64926ffea07708724e7c76815af
Ronald Kosgei <kosgeironald4@gmail.com>
2023-06-13 10:28:32 +0300

    Student-Sales-Management

---
+++ src/main/java/com/customermicroservice/entity/Customer.java
@@ -0,0 +1,43 @@
+package com.customermicroservice.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "tbl_customer")
+public class Customer {
+
+    @Id
+    @Column("customer_id")
+    private Long customerId;
+
+    @Column("first_name")
+    private String firstName;
+
+    @Column("last_name")
+    private String lastName;
+
+    @Column("email")
+    private String email;
+
+    @Column("phone_number")
+    private String phoneNumber;
+
+    @Column("address")
+    private String address;
+
+    @Column("city")
+    private String city;
+
+    @Column("order_id")
+    private Long orderId;
+
+}
